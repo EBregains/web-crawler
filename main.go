0c87c9974984d982120aab41438a966f55163540
Emiliano Bregains <ebregains@gmail.com>
2024-12-20 17:59:52 -0300

    Add argument handling for website crawling

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(args) >= 3 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	baseUrl := args[1]
+	fmt.Printf("starting crawl of: %s", baseUrl)
+	os.Exit(0)
 }

f3ae903de7787dec7871abb473b1d7e0764d07c0
Emiliano Bregains <ebregains@gmail.com>
2024-12-26 11:23:35 -0300

    Refactor main function to utilize crawlPage and remove HTML retrieval logic

--- main.go
+++ main.go
@@ -18,10 +18,17 @@ func main() {
 	baseUrl := os.Args[1]
 	fmt.Printf("starting crawl of: %s", baseUrl)
 
-	htmlBody, err := getHTML(baseUrl)
-	if err != nil {
-		fmt.Println(err)
+	// htmlBody, err := getHTML(baseUrl)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	pages := make(map[string]int)
+	crawlPage(baseUrl, baseUrl, pages)
+
+	fmt.Println("Crawl complete")
+	for url, count := range pages {
+		fmt.Printf("%s: %d\n", url, count)
 	}
-	fmt.Println(htmlBody)
+
 	os.Exit(0)
 }
